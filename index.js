e48f3b4bf51ff993a15fff7b63bc2b556695a121
nb13nb <164217882+nb13nb@users.noreply.github.com>
2024-05-21 20:08:28 +0400

    Create index.js
    .

---
+++ index.js
@@ -0,0 +1,23 @@
+// Task - 1
+let x = 100;
+if (x < 50) {
+  console.log("ნაკლებიკა 50 -ზე");
+} else if (x > 20) {
+  console.log("მეტია 20 - ზე");
+} else console.log("შეცდომა");
+
+// Task - 2
+
+let name = "მარიამ";
+
+console.log(name == "მარიამ" ? "True" : "False");
+
+// Task - 3
+
+switch (name) {
+  case "მარიამ":
+    console.log("True");
+    break;
+  default:
+    console.log("False");
+}
